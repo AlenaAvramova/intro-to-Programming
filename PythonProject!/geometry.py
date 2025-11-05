df8bef77ccc3035a0ae3fc11138bd48ee3c2fb3c
AlenaAvramova <aavramova@tu-sofia.bg>
2025-11-05 17:00:21 +0200

    Add files via upload
    Finding area of square and rectangle

---
+++ PythonProject!/geometry.py
@@ -0,0 +1,5 @@
+def area_of_square(side_lenght):
+    return side_lenght * side_lenght
+
+def area_of_rectangle(side_a, side_b):
+    return side_a * side_b
---
+++ PythonProject!/main.py
@@ -0,0 +1,4 @@
+import geometry
+print(geometry.area_of_square(5))
+
+print(geometry.area_of_rectangle(6,8))
